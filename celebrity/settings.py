ea37fe83588d3a63ca1f1a6c0df7584d7c21701e
goldsoft1206 <icbsoft87@gmail.com>
2014-03-18 21:24:16 +0800

    added base template

--- celebrity/settings.py
+++ celebrity/settings.py
@@ -80,3 +80,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "templates"),
+)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
+try:
+    from local_settings import *
+except:
+    pass
+
--- celebrity/urls.py
+++ celebrity/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'celebrity.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+    (r'^$', TemplateView.as_view(template_name="index.html")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
