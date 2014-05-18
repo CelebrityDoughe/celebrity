ce764cade09d0461a9b9063ac0e5c56bc386d82a
Kapor <smokingkapor@gmail.com>
2014-05-18 14:03:16 +0800

    Create userProfile when new user is registered

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 from django.utils.translation import ugettext as _
 
 from userena.models import UserenaBaseProfile
@@ -12,3 +13,13 @@ class UserProfile(UserenaBaseProfile):
     """
     user = models.OneToOneField(User, unique=True, verbose_name=_('user'),
                                 related_name='my_profile')
+
+
+def create_user_profile(sender, instance, created, *args, **kwargs):
+    """
+    Create related user profile object when new user is registered
+    """
+    if created:
+        UserProfile(user=instance).save()
+
+post_save.connect(create_user_profile, User)
