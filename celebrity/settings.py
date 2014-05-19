aaa0ba9123196693ca1cb694339a38dcea225111
Kapor <smokingkapor@gmail.com>
2014-05-19 18:17:00 +0800

    Update MEDIA settings

--- celebrity/settings.py
+++ celebrity/settings.py
@@ -95,6 +95,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/static/media/'
+STATIC_ROOT = os.path.join(STATIC_ROOT, 'media')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, "templates"),
