585e4ea594ec176f3b9e70a7970ca5ffbf5eee48
Kapor <smokingkapor@gmail.com>
2014-07-02 10:54:03 +0800

    Display different image for the rating

--- rating/static/js/rate.js
+++ rating/static/js/rate.js
@@ -3,6 +3,19 @@ $(document).ready(function() {
         number : 10,
         starOff : '/static/images/rate-hover.png',
         starOn  : '/static/images/rate.png',
+        iconRange: [
+            {range: 1, on: '/static/images/1.png', off: '/static/images/0.png' },
+            {range: 2, on: '/static/images/2.png', off: '/static/images/0.png' },
+            {range: 3, on: '/static/images/3.png', off: '/static/images/0.png' },
+            {range: 4, on: '/static/images/4.png', off: '/static/images/0.png' },
+            {range: 5, on: '/static/images/5.png', off: '/static/images/0.png' },
+            {range: 6, on: '/static/images/1.png', off: '/static/images/0.png' },
+            {range: 7, on: '/static/images/2.png', off: '/static/images/0.png' },
+            {range: 8, on: '/static/images/3.png', off: '/static/images/0.png' },
+            {range: 9, on: '/static/images/4.png', off: '/static/images/0.png' },
+            {range: 10, on: '/static/images/5.png', off: '/static/images/0.png' }
+        ],
+        hints: ['Score 1', 'Score 2', 'Score 3', 'Score 4', 'Score 5', 'Score 6', 'Score 7', 'Score 8', 'Score 9', 'Score 10'],
         click: function(score, evt) {
             $('#rate_score').val(score);
             $('#top_message').show();
